c914502207e3165f28915ea7b49511f5eb845f48
Nathaniel Meyer <nmeyer1134@gmail.com>
2024-04-19 13:41:28 -0600

    fixed kevvie's blunder

--- 2024-Final/Assets/Nathaniel/Scripts/PooledObject.cs
+++ 2024-Final/Assets/Nathaniel/Scripts/PooledObject.cs
@@ -15,38 +15,14 @@ public class PooledObject : MonoBehaviour
         objPool = pool;
     }
 
-    //When there are no cameras pointing at the object, release it from the pool
-    private void OnBecameInvisible()
-    {
-        if (gameObject.activeSelf)
-        {
-            StartCoroutine(DespawnTimer());
-        }
-    }
-
-    //If you see it again before it despawns, it cancels the timer
-    private void OnBecameVisible()
-    {
-        if (gameObject.activeSelf)
-        {
-            StopAllCoroutines();
-        }
-    }
-
     //This should (hopefully) stop errors when you exit play mode
     private void OnApplicationQuit()
     {
         objPool.Release(this);
     }
 
-    private void ReleaseObject()
+    public void ReleaseObject()
     {
-        objPool.Release(this);
-    }
-
-    IEnumerator DespawnTimer()
-    {
-        yield return new WaitForSeconds(3);
         objPool.Release(this);
     }
 }
